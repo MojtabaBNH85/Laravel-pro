de92bb5d0aa27c739274522d16a37611101d9084
MojtabaBNH85 <bnhmojtaba@gmail.com>
2024-08-01 17:47:08 +0330

    add route single article and fix his controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function single(Article $article){
-
-        dd($article);
         return view('admin.articles.show' , compact('article'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\Admin\ArticleController;
-use App\Http\Controllers\ArticleController as ControllersArticleController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Home\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class , 'home'])->name('home');
 
 Route::get('/about', [HomeController::class , 'about'])->name("about");
+
+Route::get('/articles/{article}', [ArticleController::class , 'single']);
 
 Route::resource('admin/articles' , 'App\Http\Controllers\Admin\ArticleController');
