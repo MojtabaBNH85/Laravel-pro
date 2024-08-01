b52302953e24feb27a080393af3bf8ea639ea802
MojtabaBNH85 <bnhmojtaba@gmail.com>
2024-08-01 18:04:29 +0330

    create article factory and delete sluggable

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,24 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
 
 class Article extends Model
 {
     use HasFactory;
-    use sluggable;
     protected $fillable = ['title', 'slug' ,'body'];
 
     public function getRouteKeyName()
     {
         return 'slug';
     }
-    public function sluggable(): array
-    {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-    }
+
 }
