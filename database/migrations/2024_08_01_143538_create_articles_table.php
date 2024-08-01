b8eba72bd9912d67671fbc55c5fb4aed242c7c47
MojtabaBNH85 <bnhmojtaba@gmail.com>
2024-08-01 18:45:04 +0330

    edite my seeder and  migarate

--- database/migrations/2024_08_01_143538_create_articles_table.php
+++ database/migrations/2024_08_01_143538_create_articles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('article', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title' , '50');
             $table->string('slug' , 100);
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('article');
+        Schema::dropIfExists('articles');
     }
 };
